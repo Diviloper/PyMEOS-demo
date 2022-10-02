ef19981bdccb096b30620f95344837d8edd49d05
Diviloper <vdiviloper@gmail.com>
2022-10-03 00:25:12 +0200

    Add demo to git

---
+++ PyMEOS-MobilityDB/demo-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+export PGPASSWORD='docker'
+
+psql -h mdb-test -p 5432 -U docker -d mobilitydb -c "DROP DATABASE IF EXISTS demo;"
+psql -h mdb-test -p 5432 -U docker -d mobilitydb -c "CREATE DATABASE demo;"
+psql -h mdb-test -p 5432 -U docker -d demo -c "CREATE EXTENSION MobilityDB CASCADE;"
+psql -h mdb-test -p 5432 -U docker -d demo -c "CREATE TABLE Trips(vehicle INTEGER,day DATE,seq INTEGER,trip tgeompoint);"
+
+python3 /pymeos-demo/load_db.py
+rm /pymeos-demo/trips.csv
+rm /pymeos-demo/load_db.py
+
+cd /pymeos-demo
+jupyter notebook --allow-root --ip=0.0.0.0
---
+++ PyMEOS-MobilityDB/load_db.py
@@ -0,0 +1,25 @@
+from pymeos.db import MobilityDB
+from pymeos import Temporal, pymeos_initialize, pymeos_finalize
+import pandas as pd
+
+host = 'mdb-test'
+port = 5432
+db = 'demo'
+user = 'docker'
+password = 'docker'
+
+pymeos_initialize()
+
+connection = MobilityDB.connect(host=host, port=port, database=db, user=user, password=password)
+cursor = connection.cursor()
+
+trips = pd.read_csv('./trips.csv', converters={'trip': Temporal.from_hexwkb})
+
+for trip in trips.iterrows():
+    t = trip[1]
+    cursor.execute(f"INSERT INTO Trips(vehicle, day, seq, trip) "
+                   f"VALUES ({t['vehicle']}, '{t['day']}', '{t['seq']}', '{t['trip']}');")
+
+connection.commit()
+cursor.close()
+pymeos_finalize()
